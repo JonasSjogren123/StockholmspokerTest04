bbd31f505a1170141ce2ff25191578f64d2046b0
Jonas Sjogren <jonas.sjogren@iths.se>
2022-09-25 19:20:20 +0200

    First Commit StockholmspokerTest04

---
+++ app/src/main/java/com/example/stockholmspokertest04/PlayingCard.kt
@@ -0,0 +1,14 @@
+package com.example.stockholmspokertest04
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayingCard {
+    lateinit var imageFace :ImageView
+    lateinit var
+}
