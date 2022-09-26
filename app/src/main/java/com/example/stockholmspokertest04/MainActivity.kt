91771190351cecf8dd82aa609e9639b9bef9c648
Jonas Sjogren <jonas.sjogren@iths.se>
2022-09-26 08:03:35 +0200

    Declared indtance of PlayingCardDeck in AminActivity

--- app/src/main/java/com/example/stockholmspokertest04/MainActivity.kt
+++ app/src/main/java/com/example/stockholmspokertest04/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
 
-
-
+    val playingCardDeck = PlayingCardDeck()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+playingCardDeck.shuffle()
+
     }
 }
